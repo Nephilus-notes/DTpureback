a7540271931166d025f8190a75680cab90392502
Nephilus-notes <114515979+Nephilus-notes@users.noreply.github.com>
2023-05-16 14:52:29 -0500

    Swagger

--- Program.cs
+++ Program.cs
@@ -39,7 +39,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy(name: MyAllowSpecificOrigins,
         policy =>
-        { //WithOrigins("http://localhost:4200/game")
+        { 
                                 policy.AllowAnyHeader()
                                 .AllowAnyOrigin()
                                 .AllowAnyMethod(); 
